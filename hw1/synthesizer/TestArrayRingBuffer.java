49e1d9160f1ffb1e52e92f361f908add22c247ce
ShenRong <2756345706@qq.com>
2024-11-01 15:17:47 +0800

    11.1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,12 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(3);
+        //arb.dequeue();
+        arb.enqueue(10);
+        arb.enqueue(20);
+        arb.enqueue(30);
+        //arb.enqueue(30);
     }
 
     /** Calls tests for ArrayRingBuffer. */
